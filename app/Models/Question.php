e046437042a14a4b0c7d4daa7b545200a391f662
minhtn143 <minhtn143@gmail.com>
2020-07-03 09:32:04 +0700

    declare Question Model

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $table = "questions";
+
+    protected $fillable = [
+        "question_content"
+    ];
+
+    public function answer()
+    {
+        return $this->hasMany('App\Models\Answer', 'question_id', 'id');
+    }
+
 }
